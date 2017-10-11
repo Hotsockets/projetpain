8b487ba2fd249228696441ec2b686e39df2a67cf
Balthazar Caillaud <balthacaillaud@hotmail.fr>
2017-10-11 17:51:50 +0200

    branche clean

--- js/script.js
+++ js/script.js
@@ -25,31 +25,3 @@ overlay.addEventListener('click', function(e) {
 
 	this.parentNode.classList.remove(activatedClass);
 });
-
-
-  <!--Modal -->
-var modal = document.getElementById('myModal');
-
-document
-  .querySelectorAll('.lighboxImage')
-  .forEach(function(el){
-    el.onclick = function(){
-        modal
-          .style
-          .display = "block";
-
-        document
-          .getElementById("img01")
-          .src = this.src;
-
-        document.getElementById("caption").innerHTML = this.alt;
-    }
-  });
-
-// When the user clicks on <span> (x), close the modal
- document
-  .getElementsByClassName("modal")[0]
-  .onclick = function() {
-
-    modal.style.display = "none";
-}

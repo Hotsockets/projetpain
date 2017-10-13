87b92c452348fc1c24de54ee2d2b0dd8a2e655d1
Balthazar Caillaud <balthacaillaud@hotmail.fr>
2017-10-13 09:52:50 +0200

    Fichiers html mis à jour + ajouts de fichiers js pour chaque page

--- js/accueil.js
+++ js/accueil.js
@@ -27,34 +27,6 @@ overlay.addEventListener('click', function(e) {
 });
 
 
-  <!--LIGHTBOX -->
-var modal = document.getElementById('myModal');
-
-document
-  .querySelectorAll('.lighboxImage')
-  .forEach(function(el){
-    el.onclick = function(){
-        modal
-          .style
-          .display = "block";
-
-        document
-          .getElementById("img01")
-          .src = this.src;
-
-        document.getElementById("caption").innerHTML = this.alt;
-    }
-  });
-
-// When the user clicks on <span> (x), close the modal
- document
-  .getElementsByClassName("modal")[0]
-  .onclick = function() {
-		if (document.getElementsByClassName)
-    modal.style.display = "none";
-}
-
-
 $(document).scroll(function(){ // écoute quand on scroll
 
 	 // Valeur en Px de la position actuelle du navigateur
